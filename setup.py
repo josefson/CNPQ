8da6d523a4d5007cb2436882de2097676920d565
Josefson Fraga <josefson@users.noreply.github.com>
2018-05-02 14:37:30 -0400

    transforming lattes into a package to pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lattes',
+      version='0.1',
+      description='Library for scraping and downloading lattes system',
+      url='',
+      author='josefson',
+      author_email='josefson.br@gmail.com',
+      license='MIT',
+      packages=['lattes'],
+      zip_safe=False)
